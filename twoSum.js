5583d54bbd0a5dd42fd919c8f3ce3e73f80808fc
tgulmine <tgulmine@gmail.com>
2023-09-21 03:25:04 -0300

    two sum

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (i = 0; i < nums.length; i++) {
+      for (j = 0; j < nums.length; j++) {
+        if (i === j) continue;
+
+        if (nums[i] + nums[j] === target) {
+          return [i, j]
+        }
+      }
+    }
+};
