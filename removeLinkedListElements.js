b7fa7a747448b555b7efdfea006abef2d3305430
tgulmine <tgulmine@gmail.com>
2024-02-06 16:35:38 -0300

    remove linked list elements

---
+++ removeLinkedListElements.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (!head) return head
+
+    if (head.val == val) return removeElements(head.next, val)
+
+    head.next = removeElements(head.next, val)
+    return head
+};
